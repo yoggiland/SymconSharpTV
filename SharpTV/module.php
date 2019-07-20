68114690149d5ecbd81add7c6970758eb1d356a5
yoggiland <jeh_hansen@yahoo.com>
2019-07-21 01:14:54 +0200

    misc

--- SharpTV/module.php
+++ SharpTV/module.php
@@ -158,7 +158,7 @@ class SharpTV extends IPSModule // Sharp Aquos TV
         $socket = stream_socket_client("{$ip}:{$port}", $errno, $errstr, 3);
         
         if (!$socket) {
-            throw new Exception(pht('Failed to connect, #%d: %s', $errno, $errstr));
+            IPS_LogMessage("SharpTV", "Could not connect to socket ");
          }
         
         if($socket) {
